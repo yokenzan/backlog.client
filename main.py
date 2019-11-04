440359a488c77f8d7ff897c996e93e3955fcd35d
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-11-05 00:08:05 +0900

    fix main process using usecase classes

--- main.py
+++ main.py
@@ -2,51 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import json
-from typing import Union, List
 
-from src.factories.issue_factory import *
-from src.api                     import Client
-from src.config                  import Config
-from src.models                  import *
+from src.api       import Client
+from src.config    import Config
+from src.usercases import *
 
 
-class IssuePrinter:
-
-    def sprint(self, issue: Union[Issue, List[Issue]]) -> str:
-        if not isinstance(issue, Issue):
-            return '\n'.join([self.sprint(i) for i in issue])
-
-        indentation = ' ' * 8
-        separator   = '-' * 100
-        description = '\n'.join([indentation + i for i in issue.description.split('\n')])
-
-        return (
-            f'\n{separator}\n'
-            f'〇 {issue.summary}\n\n'
-            f'## 状態:\n{indentation}{str(issue.status)}\n\n'
-            f'## 種別:\n{indentation}{str(issue.issueType)}\n\n'
-            f'## 内容:\n{description}\n\n'
-            f'## 担当:\n{indentation}{str(issue.assignee)}\n'
-        )
-
-    def sprint_row(self, issue: Issue) -> str:
-        keyId   = issue.keyId
-        summary = issue.summary[0:48] + '..'
-
-        return f'{keyId:<5} - {summary}'
-
-
-class UpdateStatus:
-
-    def __init__(self, timestamp: str, user: User):
-        self.timestamp = timestamp
-        self.user      = user
-
-    def __str__(self):
-        return 'UpdateStatus: ' + ', '.join([
-            self.timestamp,
-            str(self.user),
-        ])
+# class IssuePrinter:
+#
+#     def sprint(self, issue: Union[Issue, List[Issue]]) -> str:
+#         if not isinstance(issue, Issue):
+#             return '\n'.join([self.sprint(i) for i in issue])
+#
+#         indentation = ' ' * 8
+#         separator   = '-' * 100
+#         description = '\n'.join([indentation + i for i in issue.description.split('\n')])
+#
+#         return (
+#             f'\n{separator}\n'
+#             f'〇 {issue.summary}\n\n'
+#             f'## 状態:\n{indentation}{str(issue.status)}\n\n'
+#             f'## 種別:\n{indentation}{str(issue.issueType)}\n\n'
+#             f'## 内容:\n{description}\n\n'
+#             f'## 担当:\n{indentation}{str(issue.assignee)}\n'
+#         )
+#
+#     def sprint_row(self, issue: Issue) -> str:
+#         keyId   = issue.keyId
+#         summary = issue.summary[0:48] + '..'
+#
+#         return f'{keyId:<5} - {summary}'
+#
+#
+# class UpdateStatus:
+#
+#     def __init__(self, timestamp: str, user: User):
+#         self.timestamp = timestamp
+#         self.user      = user
+#
+#     def __str__(self):
+#         return 'UpdateStatus: ' + ', '.join([
+#             self.timestamp,
+#             str(self.user),
+#         ])
 
 
 
@@ -70,32 +68,28 @@ class UpdateStatus:
 
 with open('./config.json') as f: config = Config(json.load(f))
 
-client   = Client(config)
+client = Client(config)
 
 
-issue_type_factory = IssueTypeFactory()
-issue_types        = client.get_issue_types()
-[print(issue_type_factory.generate(i).to_list_item()) for i in issue_types]
-
-print('-' * 50)
-
-
-status_factory = IssueStatusFactory()
-statuses       = client.get_statuses()
-[print(status_factory.generate(i).to_list_item()) for i in statuses]
+get_issue_types = GetIssueTypes(client, IssueTypeFactory())
+[print(i.to_list_item()) for i in get_issue_types.handle()]
 
 print('-' * 50)
 
 
-user_factory = UserFactory()
-users        = client.get_users()
-[print(user_factory.generate(i).to_list_item()) for i in users]
+get_issue_statuses = GetIssueStatuses(client, IssueStatusFactory())
+[print(i.to_list_item()) for i in get_issue_statuses.handle()]
 
 print('-' * 50)
 
 
-priority_factory = PriorityFactory()
-priorities       = client.get_priorities()
-[print(priority_factory.generate(i).to_list_item()) for i in priorities]
+get_users = GetUsers(client, UserFactory())
+[print(i.to_list_item()) for i in get_users.handle()]
+
+print('-' * 50)
+
+
+get_priorities = GetPriorities(client, PriorityFactory())
+[print(i.to_list_item()) for i in get_priorities.handle()]
 
 print('-' * 50)
