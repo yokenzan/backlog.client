2862a699e7cd18aa1f4c05d30bdd78d8c5b7b40f
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-12-26 15:37:02 +0900

    modify signature of Project class property from `projectKey` to `key`

--- src/models/project.py
+++ src/models/project.py
@@ -2,15 +2,15 @@
 
 class Project:
 
-    def __init__(self, id: int, projectKey: str, name: str):
-        self.id         = id
-        self.projectKey = projectKey
-        self.name       = name
+    def __init__(self, id: int, key: str, name: str):
+        self.id   = id
+        self.key  = key
+        self.name = name
 
     def __str__(self):
         return 'Project: ' + ', '.join([
             str(self.id),
-            self.projectKey,
+            self.key,
             self.name,
         ])
 
