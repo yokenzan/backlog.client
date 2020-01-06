d1b0083ce8eb53c4b4ffc8ec3a1e15e099bf44db
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-01-06 16:27:46 +0900

    update __init__.py (fogotten change of commit 9b61fcf)

--- src/usecases/__init__.py
+++ src/usecases/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from .get_issue_statuses import *
-from .get_issue_types    import *
-from .get_priorities     import *
-from .get_users          import *
-from .launch_editor      import *
+from .get_issue_statuses    import *
+from .get_issue_types       import *
+from .get_priorities        import *
+from .get_users             import *
+from .edit_file_with_editor import *
 
