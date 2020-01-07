ff055584dcd22c095c995ce3677c6f11bd894843
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-01-07 12:23:13 +0900

    create unit test for EditFileWithEditor

---
+++ tests/unit/usecases/test_edit_file_with_editor.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from unittest.mock import Mock
+
+from src.usecases import EditFileWithEditor
+from src.usecases import EditorDetector
+from src.usecases import SubProcessRunner
+
+class EditFileWithEditorTest(unittest.TestCase):
+    def test_edit_specified_file_with_detected_editor(self):
+        editor = 'detected editor command'
+
+        detector = Mock(spec=EditorDetector)
+        detector.detect.return_value = editor
+
+        runner = Mock(spec=SubProcessRunner)
+
+        filename = 'edit filename'
+
+        EditFileWithEditor(detector, runner).handle(filename)
+
+        runner.run.assert_called_once_with([editor, filename])
