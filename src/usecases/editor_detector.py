159e3c6d965aaf5f52c469122780cf5768670e59
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-01-07 11:56:08 +0900

    create class EditorDetector

---
+++ src/usecases/editor_detector.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import os
+from src.config          import Config
+
+from .sub_process_runner import SubProcessRunner
+
+
+class EditorDetector:
+    '''
+    Detects editor launcher command.
+
+    Attributes
+    ----------
+    config : Config
+        Configuration values.
+    sub_process_runner : SubProcessRunner
+        An object running sub procces.
+    '''
+
+    def __init__(
+        self,
+        config: Config,
+        sub_process_runner: SubProcessRunner
+    ):
+        self.config             = config
+        self.sub_process_runner = sub_process_runner
+
+    def detect(self) -> str:
+        '''
+        Detects editor launcher command.
+
+        Returns
+        -------
+        str
+            Returns a command string treated as an editor launcher.
+        '''
+        preferred_editor = self.config.get_editor()
+
+        if preferred_editor is not None:
+            return preferred_editor
+
+        return os.getenv(
+            'EDITOR',
+            self.sub_process_runner.run(['which', 'vi'])
+        )
---
+++ tests/unit/usecases/test_editor_detector.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+import subprocess
+import unittest
+
+from src.config   import Config
+from src.usecases import EditorDetector
+from src.usecases import SubProcessRunner
+
+class EditorDetectorTest(unittest.TestCase):
+    def test_config_is_prior_to_others(self):
+        config_editor   = 'configured editor'
+        config          = Config({ 'editor': config_editor })
+        detector        = EditorDetector(config, SubProcessRunner())
+
+        self.assertEqual(config_editor, detector.detect())
+
+    def test_env_has_secondaly_highest_priority(self):
+        os.environ['EDITOR'] = 'ENV EDITOR'
+
+        self.assertEqual(
+            os.getenv('EDITOR'),
+            EditorDetector(Config({}), SubProcessRunner()).detect()
+        )
+
+    def test_vi_path_is_returnd_if_neigther_of_config_nor_env_is_exists(self):
+        os.environ.pop('EDITOR')
+
+        self.assertEqual(
+            subprocess.check_output(['which', 'vi']).decode().strip(),
+            EditorDetector(Config({}), SubProcessRunner()).detect()
+        )
